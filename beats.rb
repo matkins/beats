3ee843d516886a205b801968821c7f0ed627c2b6
Matt Atkins <matt@yoomee.com>
2011-04-02 16:32:41 +0100

    Assume 4/4. Allow entry of tempo

--- beats.rb
+++ beats.rb
@@ -15,11 +15,16 @@ puts "Loading beats..."
 
 include MIDIator::Drums
 
-
+four_four = [ BassDrum1,
+  [BassDrum2, SnareDrum1],
+  BassDrum1,
+  [BassDrum1, SnareDrum1, SnareDrum2]
+]
 
 def get_time_signature
-  print "Enter a time signature, (e.g. 3/4): "
+  print "Enter a time signature, [4/4]: "
   tss = gets
+  return [4,4] if tss.strip == ''
   num, den = tss.split('/').collect{|n| n.to_i}
   if num && den && num > 0 && den > 0
     [num,den]
@@ -29,13 +34,16 @@ def get_time_signature
 end
 
 def get_tempo
-  puts "Tap out two bars with the Enter key"
+  print "Enter a tempo in bpm or tap out two bars:"
   prev = nil
   beats = []
   (bpb * 2).times do |counter|
-    gets
-    beat(0.1, counter)
+    tempoin = gets
+    if !(bpm = tempoin.strip.to_i).zero?
+      return bpm, Time.now
+    end
     now = Time.now
+    beat(0.1, counter)
     if prev
       beats << now - prev 
       counter += 1
@@ -47,13 +55,16 @@ def get_tempo
     puts "That beat was a bit irregular, please try again!"
     get_tempo
   else
-    (60/average).to_i
+    return (60/average).to_i, prev
   end
 end
 
+def beat_length
+  60 / @tempo.to_f
+end
 
 def beat(length, beat_num)
-  if beat_num % @time_signature[0] == 0
+  if beat_num % bpb == 0
     @midi.play BassDrum2, length, 10
   else
     @midi.play LowTom1, length, 10
@@ -67,13 +78,30 @@ end
 # Begin here....
 
 @time_signature = get_time_signature
-@tempo = get_tempo
+@tempo, last_beat = get_tempo
 
 
 puts "You tapped at #{@tempo} beats per minute."
-sleep(0.5)
 puts "Here goes..."
-(bpb * 4).times do |i|
-  beat(60/@tempo.to_f, i)
+while Time.now - last_beat < beat_length do ; end
+
+loop do #4.times do
+  bpb.times do |i|
+    @midi.play four_four[i], beat_length/2, 10
+    @midi.play ClosedHiHat, beat_length/2, 10
+  end
 end
-puts ""
+
+
+#More accurate ?
+# start = Time.now
+# now = nil
+# (bpb * 40).times do |i|
+#   loop do
+#     now = Time.now
+#     break if now - last_beat >= beat_length
+#   end
+#   beat(beat_length/2, i)
+#   puts "#{(now - start) - ((i + 1) * beat_length)}"
+#   last_beat = now
+# end
